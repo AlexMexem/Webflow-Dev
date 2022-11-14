9646314b94d2fed349a864a825249046b52ea85a
Alexis <alexis.basnett@mexem.com>
2022-11-14 09:40:01 +0200

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("hello");
+
+window.fsAttributes = window.fsAttributes || [];
+window.fsAttributes.push([
+  'cmsload',
+  (listInstances) => {
+    console.log('cmsload Successfully loaded!');
+
+    // The callback passes a `listInstances` array with all the `CMSList` instances on the page.
+    const [listInstance] = listInstances;
+
+    // The `renderitems` event runs whenever the list renders items after switching pages.
+    listInstance.on('renderitems', (renderedItems) => {
+      console.log(renderedItems);
+    });
+  },
+]);
+
+console.log("end")
